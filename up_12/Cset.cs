a28acd019a55f0c6c5da2e4dfd01b9bf0672abd5
LyubovTatur <71625191+LyubovTatur@users.noreply.github.com>
2021-03-04 10:42:34 +0300

    done

---
+++ up_12/Cset.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace up_12
+{
+    class Cset
+    {
+        public delegate void AccountHandler(string message);
+        public event AccountHandler Notify;              // 1.Определение события
+
+        private dynamic[] list;
+
+        public Cset(dynamic[] list)
+        {
+            this.list = list;
+        }
+        public static Cset operator +(Cset c1, Cset c2)
+        {
+            return new Cset(c1.list.Concat(c2.list).ToArray());
+        }
+        //public static Cset operator -(Cset c1, Cset c2)
+        //{
+        //    return new Cset(c1.list.Except(c2.list).ToArray());
+        //}
+        public static bool operator >=(Cset c1, Cset c2)
+        {
+            if (c1.Int() >= c2.Int())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator <=(Cset c1, Cset c2)
+        {
+            if (c1.Int() <= c2.Int())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public int Int()
+        {
+            return list.Length;
+        }
+        public static Cset operator &(Cset c1, Cset c2)
+        {
+
+            return new Cset(c1.list.Except(c2.list).ToArray());
+
+
+        }
+
+        public  dynamic this[int i]
+        {
+            get
+            {
+                return list[i];
+            }
+            set
+            {
+                list[i] = value;
+            }
+        }
+        public void DelElem(int index)
+        {
+            Notify?.Invoke($"был удален элем {list[index]}");
+            list = list.Except(new dynamic[] { list[index] }).ToArray();
+        }
+        ~Cset()
+        {
+            Console.WriteLine( "Элемент удален.");
+        }
+
+    }
+}
+
+//  Класс – множество Сset.Дополнительно перегрузить следующие операции: + –
+//  объединение множеств; &lt;=,&gt;= – сравнение множеств; int()– количество элементов
+//  множества; &amp;– пересечение.Удаление элемента из множества.
+//  Генерацию события по удалению элемента из массива. В обработчике указать значение
+//  удаленного элемента
---
+++ up_12/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace up_12
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Cset c1 = new Cset(new dynamic[] { 2 });
+
+            Cset c2 = new Cset(new dynamic[] { 4 });
+            Cset c3 = c1 + c2;
+            Console.WriteLine(c3[0]);
+            c3.Notify += DisplayMessage;
+            c3.DelElem(0);
+            // Добавляем обработчик для события Notify
+            Console.WriteLine("Введите n");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(c3[0]);
+            Cset[] csets = new Cset[]
+            {
+                new Cset (new dynamic[]{ 3,false,"efd"}),
+                new Cset (new dynamic[]{ 4,3,3}),
+                new Cset (new dynamic[]{ true,"joopa","efd"}),
+                new Cset (new dynamic[]{ 6,5,"54"}),
+            };
+            for (int i = 0; i < csets.Length; i++)
+            {
+                if (csets[i].Int() == n)
+                {
+                    Console.WriteLine($"найден сет с размерностью n - {i}");
+                }
+            }
+            int max = 0;
+            for (int i = 0; i < csets.Length; i++)
+            {
+                if (csets[i].Int()>max)
+                {
+                    max = csets[i].Int();
+                }
+            }
+            for (int i = 0; i < csets.Length; i++)
+            {
+                if (csets[i].Int() == max)
+                {
+                    Console.WriteLine($"{i} сет имеет макс колво элемов");
+                }
+            }
+
+        }
+        private static void DisplayMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+}
+//  создать объект CollectionType&lt; T & gt;, содержащий массив обобщённых
+//  коллекций, запросы – найти коллекции размера n; найти максимальную
+//  и минимальную коллекцию в массиве по количеству элементов.
+//  Создать полный клон объекта, учитывая, что каждый элемент
+//  коллекции, реализует интерфейс IClonable. Обобщенная коллекция –
+//  LinkedList&lt; T & gt;.
